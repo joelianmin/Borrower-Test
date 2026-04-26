0657024a6bc7b38fda5c6710a44d042b36e15b69
Joe Lian Min <joelianmin@gmail.com>
2026-04-26 17:16:04 +0700

    update nama class cucumber test supaya jalan dan pom pada ndjson jadi json

--- src/test/java/com/example/CucumberTest.java
+++ src/test/java/com/example/CucumberTest.java
@@ -10,5 +10,6 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.example.steps")
-public class CucumberTestRunner {
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json")
+public class CucumberTest {
 }
